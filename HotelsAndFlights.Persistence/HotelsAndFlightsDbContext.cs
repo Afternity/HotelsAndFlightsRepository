5a4039ae4cacd5a8536bfb6e6bf143fd26c836a5
Максимов Андрей <a89995773757@gmail.com>
2025-05-11 23:39:06 +0300

    Создал заготовки для будующей реализации(DependencyInjection, DbContext, EntityTypeConfigurations). Данные вещи у меня ре раализованны полностью, только HotelsAndFlightsDbContext.cs почти готов. Работа будет происходить в PSQL

---
+++ HotelsAndFlights.Persistence/HotelsAndFlightsDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelsAndFlights.Domain;
+using HotelsAndFlights.Domain.FlightsModels;
+using HotelsAndFlights.Domain.Models;
+using HotelsAndFlights.Domain.Reservations;
+using HotelsAndFlights.Domain.UsersModels;
+
+namespace HotelsAndFlights.Persistence
+{
+    public class HotelsAndFlightsDbContext : DbContext
+    {
+        private readonly DbContextOptions<HotelsAndFlightsDbContext> _options;
+
+        public HotelsAndFlightsDbContext(DbContextOptions<HotelsAndFlightsDbContext> options) 
+            : base(options) =>
+            _options = options;
+        
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<FlightSeat> FlightSeats { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<HotelPhoto> HotelPhotos { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<FlightBooking> FlightBookings { get; set; }
+        public DbSet<HotelBooking> HotelBookings { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserType> UserTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
