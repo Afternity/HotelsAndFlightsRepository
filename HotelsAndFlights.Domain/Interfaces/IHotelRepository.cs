1aea42a490228a4088ad42d82fd051d33f08a559
Максимов Андрей <a89995773757@gmail.com>
2025-05-14 19:09:12 +0300

    Сделал HotelRepository и исправил несколько моментов в IHotelRepository

--- HotelsAndFlights.Domain/Interfaces/IHotelRepository.cs
+++ HotelsAndFlights.Domain/Interfaces/IHotelRepository.cs
@@ -5,10 +5,10 @@ namespace HotelsAndFlights.Domain.Interfaces
     public interface IHotelRepository
     {
         Task<Hotel> GetByAsync(Guid hotelId);
-        Task<Hotel> AddAsync(Hotel hotel);
-        Task<Hotel> UpdateAsync(Hotel hotel);
-        Task<Hotel> DeleteAsync(Guid hotelId);
+        Task AddAsync(Hotel hotel);
+        Task UpdateAsync(Hotel hotel);
+        Task DeleteAsync(Hotel hotel);
         Task<IList<Hotel>> GetAllAsync();
-        Task<IList<Hotel>> GetByRatingIdAsync(double rating);
+        Task<IList<Hotel>> GetByRatingAsync(double rating);
     }
 }
---
+++ HotelsAndFlights.Persistence/Data/Repositories/HotelRepository.cs
@@ -0,0 +1,52 @@
+﻿using HotelsAndFlights.Domain.Interfaces;
+using HotelsAndFlights.Domain.Models.HotelModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelsAndFlights.Persistence.Data.Repositories
+{
+    public class HotelRepository : IHotelRepository
+    {
+        private readonly HotelsAndFlightsDbContext _context;
+
+        public HotelRepository(HotelsAndFlightsDbContext context) => _context = context;
+
+        public async Task<Hotel> GetByAsync(Guid hotelId)
+        {
+            return await _context.Hotels.FindAsync(hotelId);
+        }
+
+        public async Task AddAsync(Hotel hotel)
+        {
+             await _context.Hotels.AddAsync(hotel);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Hotel hotel)
+        {
+            _context.Hotels.Update(hotel);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Hotel hotel)
+        {
+            _context.Hotels.Remove(hotel);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IList<Hotel>> GetAllAsync()
+        {
+            return await _context.Hotels.OrderBy(hotel => hotel.Name).ToListAsync();
+        }
+
+
+        public Task<IList<Hotel>> GetByRatingAsync(double rating)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
